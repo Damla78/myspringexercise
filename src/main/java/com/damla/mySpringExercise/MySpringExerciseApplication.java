ff4285698f0ab5933147aae7c163a16a3016d810
Damla Kara <damla@Damlas-MacBook-Pro.local>
2020-04-21 14:21:17 +0200

    Initial commit

---
+++ src/main/java/com/damla/mySpringExercise/MySpringExerciseApplication.java
@@ -0,0 +1,14 @@
+package com.damla.mySpringExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringExerciseApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MySpringExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damla/mySpringExercise/MySpringExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damla.mySpringExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
